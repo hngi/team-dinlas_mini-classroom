29bb240e4748804d3926f0ac4757de3f82c244f7
Toheeb Ogunbiyi <toogunbiyi@gmail.com>
2019-09-24 09:11:39 +0100

    Modify Login Markup to identify submit button

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ include 'includes/functions.php';
 */
 
 
-if (isset($_POST['Login'])) {
+if (isset($_POST['login'])) {
 
     $email_unsafe = $_POST['email'];
     $pass_unsafe = $_POST['password'];
