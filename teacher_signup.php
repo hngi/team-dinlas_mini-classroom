fb9bf4fa0e8391b29e6c8740f80127cc3d04d64e
Jude Jonathan <jonathanjude27@gmail.com>
2019-09-28 11:47:32 +0100

    test

--- teacher_signup.php
+++ teacher_signup.php
@@ -4,52 +4,52 @@
 include 'includes/db_connection.php';
 include 'includes/functions.php';
 
-if (isset($_POST['signup'])) {
+// if (isset($_POST['signup'])) {
 
-    $fullname_unsafe = $_POST['fullname'];
-    $email_unsafe = $_POST['email'];
-    $phone_unsafe = $_POST['phone'];
-    $pass_unsafe = $_POST['password'];
-    $conf_pass_unsafe = $_POST['confirm_password'];
+//     $fullname_unsafe = $_POST['fullname'];
+//     $email_unsafe = $_POST['email'];
+//     $phone_unsafe = $_POST['phone'];
+//     $pass_unsafe = $_POST['password'];
+//     $conf_pass_unsafe = $_POST['confirm_password'];
 
-    $fullname = mysqli_real_escape_string($con, $fullname_unsafe);
-    $email = mysqli_real_escape_string($con, $email_unsafe);
-    $phone = mysqli_real_escape_string($con, $phone_unsafe);
-    $password = mysqli_real_escape_string($con, $pass_unsafe);
-    $confirm_password = mysqli_real_escape_string($con, $conf_pass_unsafe);
+//     $fullname = mysqli_real_escape_string($con, $fullname_unsafe);
+//     $email = mysqli_real_escape_string($con, $email_unsafe);
+//     $phone = mysqli_real_escape_string($con, $phone_unsafe);
+//     $password = mysqli_real_escape_string($con, $pass_unsafe);
+//     $confirm_password = mysqli_real_escape_string($con, $conf_pass_unsafe);
 
-    //Validation
+//     //Validation
 
-    //check if email exists
-    $email_query = mysqli_query($con, "SELECT * FROM teachers WHERE email = '$email'") or die(mysqli_error($con));
-    $email_count = mysqli_num_rows($email_query);
+//     //check if email exists
+//     $email_query = mysqli_query($con, "SELECT * FROM teachers WHERE email = '$email'") or die(mysqli_error($con));
+//     $email_count = mysqli_num_rows($email_query);
 
-    if (!validateEmail($email)) {
-        addAlert('error', 'Invalid Email address');
-        echo "<script>document.location='teacher_signup.php'</script>";
-    } else if (strlen($password) < 6) {
-        addAlert('error', 'Password must be atleast Six (6) characters');
-        echo "<script>document.location='teacher_signup.php'</script>";
-    } else if ($password != $confirm_password) {
-        addAlert('error', 'Passwords dont Match');
-        echo "<script>document.location='teacher_signup.php'</script>";
-    } else if ($email_count > 0) {
-        addAlert('error', 'Email address already exists!');
-        echo "<script>document.location='teacher_signup.php'</script>";
-    } else {
+//     if (!validateEmail($email)) {
+//         addAlert('error', 'Invalid Email address');
+//         echo "<script>document.location='teacher_signup.php'</script>";
+//     } else if (strlen($password) < 6) {
+//         addAlert('error', 'Password must be atleast Six (6) characters');
+//         echo "<script>document.location='teacher_signup.php'</script>";
+//     } else if ($password != $confirm_password) {
+//         addAlert('error', 'Passwords dont Match');
+//         echo "<script>document.location='teacher_signup.php'</script>";
+//     } else if ($email_count > 0) {
+//         addAlert('error', 'Email address already exists!');
+//         echo "<script>document.location='teacher_signup.php'</script>";
+//     } else {
 
-        $res = mysqli_query($con, "INSERT INTO teachers SET fullname = '$fullname', password = '$password', email = '$email', phone = '$phone' ") or die(mysqli_error($con));
-        if ($res) {
-            addAlert('success', 'Registration Successful! Please Login');
-            echo "<script type='text/javascript'>document.location='teacher_signup.php'</script>";
-        } else {
-            addAlert('error', 'Something went wrong!');
-            echo "<script type='text/javascript'>document.location='teacher_signup.php'</script>";
-        }
-    }
-} else {
-    header('Location index.php');
-}
+//         $res = mysqli_query($con, "INSERT INTO teachers SET fullname = '$fullname', password = '$password', email = '$email', phone = '$phone' ") or die(mysqli_error($con));
+//         if ($res) {
+//             addAlert('success', 'Registration Successful! Please Login');
+//             echo "<script type='text/javascript'>document.location='teacher_signup.php'</script>";
+//         } else {
+//             addAlert('error', 'Something went wrong!');
+//             echo "<script type='text/javascript'>document.location='teacher_signup.php'</script>";
+//         }
+//     }
+// } else {
+//     header('Location index.php');
+// }
 
 ?>
 
