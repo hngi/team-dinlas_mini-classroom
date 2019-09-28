f197e986782a2b6e70c81f69a5e8b2cbdb802d59
Jude Jonathan <jonathanjude27@gmail.com>
2019-09-28 22:43:16 +0100

    db info added

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,10 @@
 <?php
 
 //Database Options
-$databaseHost = 'localhost';
-$databaseName = 'mini_class';
-$databaseUser = 'root';
-$databasePassword = '';
+$databaseHost = 'a5s42n4idx9husyc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306';
+$databaseName = 'usqod01dmfdpbgq0';
+$databaseUser = 'i2nur0ym9o5hc94i';
+$databasePassword = 'rzthcnliqigjkpdb';
 
 //Website
 $baseUrl = 'https://di-class.herokuapp.com';
