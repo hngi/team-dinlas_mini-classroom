ca7378de0e5f348f060d0f067be45a888c17a761
Jude Jonathan <jonathanjude27@gmail.com>
2019-09-28 12:32:33 +0100

    fix

--- student_signup.php
+++ student_signup.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Teacher Sign Up - DiClass</title>
+    <title>Student Sign Up - DiClass</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
 </head>
